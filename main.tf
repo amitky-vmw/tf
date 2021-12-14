32542c69b186e66e59867f30916d69cb41f48bf7
Amit K <77483290+amitky-vmw@users.noreply.github.com>
2021-12-14 18:49:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,17 +13,9 @@ default = "terraform-test"
 }
 
 resource "aws_instance" "machine1" {
-ami = "ami-04b9e92b5572fa0d1"
+ami = "ami-49487a35"
 instance_type = "t2.micro"
-availability_zone = "us-east-1b"
-tags = {
-"type" = var.myTag
-}
-}
-resource "aws_instance" "machine2" {
-ami = "ami-04b9e92b5572fa0d1"
-instance_type = "t2.micro"
-availability_zone = "us-east-1b"
+availability_zone = "ap-southeast-1"
 tags = {
 "type" = var.myTag
 }
